ded2e94e07683426e0b74c3186eff04ac3d11b08
jazzan <admia1>
2021-09-21 06:41:03 +0430

    adding options

--- district_sieve.c
+++ district_sieve.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#define N 1048576 
-#define N1 1024 
+#define N 1048576
+#define N0 1
+#define N1 1024
 #define N2 1048576
 #define MIL 1000000
 int arr[N];
@@ -8,9 +9,23 @@ long long prime[N];
 int pcount = 0;
 int main()
 	{
+	int option=0;
+	printf("0 -> 2^20\n1 -> 2^30\n2 -> 2^40\n");
+	scanf("%d", &option);
+	long long max_dirty = 0;
+
+	if (option == 0)
+		max_dirty = 1;
+
+	if (option == 1)
+		max_dirty = N1;
+
+	if (option == 2)
+		max_dirty = N2;
+
 	//printf("2\n");
 	long long dirty=0;
-	for(int i=0;i<N;i+=2){
+	for(int i=0;i<N;i+=1){
 		arr[i]=dirty-1;
 		}
 	for(long long i=3;i<N;i+=2){
@@ -22,14 +37,14 @@ int main()
 				{
 				arr[j]=dirty;
 				}
-			
+
 			prime[pcount]=j;
 			pcount++;
-			
+
 			}
 		}
 	int count = pcount+1;
-	for(dirty=1;dirty<N2;dirty++)
+	for(dirty=1;dirty<max_dirty;dirty++)
 		{
 		//dirty job
 		for(int i=0;i<pcount;i+=2)
@@ -48,14 +63,13 @@ int main()
 			if(arr[i]<dirty)
 				{
 				count++;
-				//printf("%lld\n", dirty*N+i);
+				// printf("%lld\n", dirty*N+i);
 				}
 			}
 		}
-	
+
 	//for(int i=0; i<pcount; i+=2)
 		//printf("%lld %lld\n",prime[i],prime[i+1]);
-	
-	printf("%d",count);
-	}
 
+	printf("%d\n",count);
+	}
